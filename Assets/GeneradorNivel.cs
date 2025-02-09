f18d2f3ec67f40677027f6549806c240eaeac7ba
unknown <carlosi.huerta@udem.edu>
2025-02-09 03:05:11 -0600

    Desaparición de segmentos?

--- Assets/GeneradorNivel.cs
+++ Assets/GeneradorNivel.cs
@@ -8,7 +8,9 @@ public class GeneradorNivel : MonoBehaviour
     [SerializeField] private float distanciaMinima;
     [SerializeField] private Transform puntoFinal;
     [SerializeField] private int cantidadInicial;
+    [SerializeField] private float distanciaDestruccion = 4000f;
     private Transform jugador;
+    private Transform ultimoPuntoFinal;
 
     private void Start(){
         jugador = GameObject.FindGameObjectWithTag("Player").transform;
@@ -16,18 +18,24 @@ public class GeneradorNivel : MonoBehaviour
         for(int i= 0; i< cantidadInicial; i++){
             GenerarParteNivel();
         }
+        ultimoPuntoFinal = puntoFinal;
     }
 
     private void Update(){
-        if(Vector2.Distance(jugador.position, puntoFinal.position)< distanciaMinima){
+        if(ultimoPuntoFinal!= null && Vector2.Distance(jugador.position, ultimoPuntoFinal.position)< distanciaMinima){
              GenerarParteNivel();
         }
+        DestruirSegmento();
     }
 
     private void GenerarParteNivel(){
         int numeroAleatorio= Random.Range(0, partesNivel.Length);
         GameObject nivel= Instantiate(partesNivel[numeroAleatorio], puntoFinal.position, Quaternion.identity);
-        puntoFinal = BuscarPuntoFinal(nivel, "PuntoFinal");
+        puntoFinal= BuscarPuntoFinal(nivel, "PuntoFinal");
+        
+        if(puntoFinal!= null){
+            ultimoPuntoFinal= puntoFinal;
+        }
     }
 
     private Transform BuscarPuntoFinal(GameObject parteNivel, string etiqueta){
@@ -40,5 +48,17 @@ public class GeneradorNivel : MonoBehaviour
             }
         }
         return punto;
+    }
+
+    private void DestruirSegmento(){
+        GameObject[] segmentos = GameObject.FindGameObjectsWithTag("Segmento");
+
+        foreach(GameObject segmento in segmentos){
+            float distancia = Vector2.Distance(jugador.position, segmento.transform.position);
+
+            if(distancia> distanciaDestruccion){
+                Destroy(segmento);
+            }
+        }
     }
 }
