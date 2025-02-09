1e0966292938a45e8593fe60cc4dded1090bc798
unknown <carlosi.huerta@udem.edu>
2025-02-09 02:26:00 -0600

    Mapa infinito, seguimiento vertical del jugador

--- Assets/GeneradorNivel.cs
+++ Assets/GeneradorNivel.cs
@@ -4,15 +4,41 @@ using UnityEngine;
 
 public class GeneradorNivel : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    [SerializeField] private GameObject[] partesNivel;
+    [SerializeField] private float distanciaMinima;
+    [SerializeField] private Transform puntoFinal;
+    [SerializeField] private int cantidadInicial;
+    private Transform jugador;
+
+    private void Start(){
+        jugador = GameObject.FindGameObjectWithTag("Player").transform;
+
+        for(int i= 0; i< cantidadInicial; i++){
+            GenerarParteNivel();
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    private void Update(){
+        if(Vector2.Distance(jugador.position, puntoFinal.position)< distanciaMinima){
+             GenerarParteNivel();
+        }
+    }
+
+    private void GenerarParteNivel(){
+        int numeroAleatorio= Random.Range(0, partesNivel.Length);
+        GameObject nivel= Instantiate(partesNivel[numeroAleatorio], puntoFinal.position, Quaternion.identity);
+        puntoFinal = BuscarPuntoFinal(nivel, "PuntoFinal");
+    }
+
+    private Transform BuscarPuntoFinal(GameObject parteNivel, string etiqueta){
+        Transform punto = null;
+
+        foreach(Transform ubicacion in parteNivel.transform){
+            if(ubicacion.CompareTag(etiqueta)){
+                punto = ubicacion;
+                break;
+            }
+        }
+        return punto;
     }
 }
